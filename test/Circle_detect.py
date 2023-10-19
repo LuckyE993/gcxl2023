0a6664ba4324f19ba20a8116c3d066457b8bfcf5
Lucky_Eureka <90081491+LuckyE993@users.noreply.github.com>
2023-10-19 17:12:33 +0800

    Delete test directory

--- test/Circle_detect.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-import cv2 as cv
-img = cv.imread('BGR_COLOR.jpg',cv.IMREAD_UNCHANGED)
-gray = 	cv.cvtColor(img,cv.COLOR_BGR2GRAY)
-cv.imshow('IMG',img)
-cv.imshow('GRAY',gray)
-cv.waitKey(0)
-cv.destroyAllWindows()
--- test/Com 样例.py
+++
@@ -1,3 +0,0 @@
-print(int(abs(2) / 1.25))
-dis = 1 * 0.75 / 1.25 * 125
-print(dis)
